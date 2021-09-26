7b9900b96096a4964def142103e716f1198af23e
Eduardo Rodrigues da Silva <eduardo.rod.s@gmail.com>
2021-09-25 21:55:29 -0300

    Adiciona docker

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM node:latest
+
+WORKDIR /usr/src/app
+
+COPY package.json ./
+COPY ./src ./src
+RUN npm install
+EXPOSE 3000
+
+CMD ["npm", "start"]
--- src/repositories/db.js
+++ src/repositories/db.js
@@ -2,13 +2,26 @@ import {Sequelize} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING,
-    {
-      dialect: 'postgres',
-      define: {
-        timestamps: true,
-      },
-      logging: (msg) => global.logger.debug(msg),
-    });
+let sequelize = null;
 
+if (process.env.NODE_ENV !== 'production') {
+  sequelize = new Sequelize(process.env.DB_CONNECTION_STRING,
+      {
+        dialect: 'postgres',
+        define: {
+          timestamps: true,
+        },
+        logging: (msg) => global.logger.debug(msg),
+      });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: '172.17.0.2',
+    port: 5432,
+    database: 'desafio4',
+    username: 'postgres',
+    password: 'mysecretpassword',
+    logging: (msg) => global.logger.debug(msg),
+  });
+}
 export default sequelize;
